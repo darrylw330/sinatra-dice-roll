a07d9e71c2d64174c7cf88849012b3aca1244b8f
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-07-26 14:52:46 +0000

    added /dice/1/20 route

--- dice.rb
+++ dice.rb
@@ -36,3 +36,14 @@ outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}. 
 "<h1>2d10</h1>
 <p>#{outcome}</p>"
 end
+
+get("/dice/1/20") do
+first_die = rand(1..20)
+second_die = rand(1..20)
+sum = first_die + second_die
+
+outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}. Get this shmoney"
+
+"<h1>1d20</h1>
+<p>#{outcome}</p>"
+end
